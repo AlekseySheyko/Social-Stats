5d59f5baaa782ff05eadde09f783c4d7e242475f
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-02-21 16:51:37 +0200

    Add account from Instagram.

--- app/src/main/java/aleksey/sheyko/staticdemo/models/instagram/WebviewAuthClient.java
+++ app/src/main/java/aleksey/sheyko/staticdemo/models/instagram/WebviewAuthClient.java
@@ -3,6 +3,7 @@ package aleksey.sheyko.staticdemo.models.instagram;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,11 +17,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
 import aleksey.sheyko.staticdemo.R;
-import aleksey.sheyko.staticdemo.app.activities.SocialActivity;
+import aleksey.sheyko.staticdemo.app.activities.MainActivity;
+import aleksey.sheyko.staticdemo.app.database.AccountDataSource;
+import aleksey.sheyko.staticdemo.models.Account;
+import aleksey.sheyko.staticdemo.models.AccountStats;
 
 public class WebviewAuthClient extends WebViewClient {
 
@@ -58,29 +64,69 @@ public class WebviewAuthClient extends WebViewClient {
         protected Void doInBackground(Void... voids) {
             try {
                 URL url = new URL(mTokenUrlString);
-                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
-                httpsURLConnection.setRequestMethod("POST");
-                httpsURLConnection.setDoInput(true);
-                httpsURLConnection.setDoOutput(true);
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(httpsURLConnection.getOutputStream());
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(urlConnection.getOutputStream());
                 outputStreamWriter.write("client_id=" + Constants.CLIENT_ID +
                         "&client_secret=" + Constants.CLIENT_SECRET +
                         "&grant_type=authorization_code" +
                         "&redirect_uri=" + Constants.CALLBACK_URL +
                         "&code=" + mRequestToken);
                 outputStreamWriter.flush();
-                String response = streamToString(httpsURLConnection.getInputStream());
-                JSONObject jsonObject = (JSONObject) new JSONTokener(response).nextValue();
-                String accessToken = jsonObject.getString("access_token"); // Here is your ACCESS TOKEN
-                String id = jsonObject.getJSONObject("user").getString("id");
-                String username = jsonObject.getJSONObject("user").getString("username"); //This is how you can get the user info. You can explore the JSON sent by Instagram as well to know what info you got in a response
-                mSharedPref.edit()
-                        .putString("access_token", accessToken)
-                        .putString("id", id)
-                        .putString("username", username)
-                        .apply();
-                mContext.startActivity(new Intent(mContext, SocialActivity.class)
-                        .putExtra("username", username));
+                String response = streamToString(urlConnection.getInputStream());
+                JSONObject responseObject = (JSONObject) new JSONTokener(response).nextValue();
+                String accessToken = responseObject.getString("access_token"); // Here is your ACCESS TOKEN
+                String id = responseObject.getJSONObject("user").getString("id");
+                String username = responseObject.getJSONObject("user").getString("username");
+
+                // Start querying user data
+                Uri.Builder builder = new Uri.Builder();
+                builder.scheme("https")
+                        .authority("api.instagram.com")
+                        .appendPath("v1")
+                        .appendPath("users")
+                        .appendPath(id)
+                        .appendQueryParameter("access_token", accessToken);
+                String urlString = builder.build().toString();
+
+                url = new URL(urlString);
+                urlConnection = (HttpsURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuilder buffer = new StringBuilder();
+                if (inputStream == null) return null;
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                }
+                response = buffer.toString();
+                responseObject = (JSONObject) new JSONTokener(response).nextValue();
+
+                JSONObject dataObject = responseObject.getJSONObject("data");
+                JSONObject countsObject = dataObject.getJSONObject("counts");
+
+                int media = countsObject.getInt("media");
+                int following = countsObject.getInt("follows");
+                int followers = countsObject.getInt("followed_by");
+
+                List<AccountStats> statsList = new ArrayList<>();
+                statsList.add(new AccountStats("followers", followers));
+                statsList.add(new AccountStats("media", media));
+                statsList.add(new AccountStats("following", following));
+
+                Account account = new Account("Instagram", username, statsList);
+
+                AccountDataSource dataSource = new AccountDataSource(mContext);
+                dataSource.create(account);
+
+                mContext.startActivity(new Intent(mContext, MainActivity.class));
             } catch (Exception e) {
                 e.printStackTrace();
             }
